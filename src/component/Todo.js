5eab192987bd1b0bc788c4275d49410800a1e0fe
Pravin <ppal@isystango.com>
2019-07-16 15:17:58 +0530

    Worked on useReducer hook.

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,8 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 
+const todoStateReducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_TODO_NAME":
+      return {
+        ...state,
+        todoName: action.payload
+      }
+    case "ADD":
+      return {
+        ...state,
+        todoList: state.todoList.concat(action.payload)
+      }
+    case "SET":
+      return {
+        ...state,
+        todoList: action.payload
+      }
+    case "REMOVE":
+      return {
+        ...state,
+        todoList: state.todoList.filter(todo => todo.id !== action.payload)
+      }
+    default:
+      return state;
+  }
+}
+
 const Todo = props => {
-  const [todoState, setTodoState] = useState({ todoName: "", todoList: [] });
+  // const [todoState, setTodoState] = useState({ todoName: "", todoList: [] });
+  const [state, dispatch] = useReducer(todoStateReducer, { todoName: "", todoList: [] })
 
   useEffect(() => {
     axios
@@ -12,46 +40,54 @@ const Todo = props => {
         for (const key in result.data) {
           todos.push({ id: key, name: result.data[key].name });
         }
-        updateState(undefined, todos);
+        dispatch({ type: 'SET', payload: todos })
+        // updateState(undefined, todos);
       })
       .catch(err => {
         console.log("[Error]", err);
       });
-  }, todoState);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, state);
 
   const inputChangeHandler = e => {
-    updateState(e.target.value);
+    dispatch({ type: 'UPDATE_TODO_NAME', payload: e.target.value })
+    // updateState(e.target.value);
   };
 
   const todoListHandler = () => {
     axios
       .post("https://react-hooks-66a58.firebaseio.com/todos.json", {
-        name: todoState.todoName
+        name: state.todoName
       })
       .then(res => {
-        updateState(
-          undefined,
-          todoState.todoList.concat({
+        dispatch({
+          type: 'ADD', payload: {
             id: res.data.name,
-            name: todoState.todoName
-          })
-        );
-        console.log("Todo Added", res);
+            name: state.todoName
+          }
+        })
+        // updateState(
+        //   undefined,
+        //   todoState.todoList.concat({
+        //     id: res.data.name,
+        //     name: todoState.todoName
+        //   })
+        // );
       })
       .catch(err => {
         console.log("Error", err);
       });
   };
 
-  const updateState = (
-    name = todoState.todoName,
-    list = todoState.todoList
-  ) => {
-    setTodoState({
-      todoName: name,
-      todoList: list
-    });
-  };
+  // const updateState = (
+  //   name = todoState.todoName,
+  //   list = todoState.todoList
+  // ) => {
+  //   setTodoState({
+  //     todoName: name,
+  //     todoList: list
+  //   });
+  // };
 
   return (
     <>
@@ -59,13 +95,13 @@ const Todo = props => {
         type="text"
         placeholder="Todo"
         onChange={inputChangeHandler}
-        value={todoState.todoName}
+        value={state.todoName}
       />
       <button type="button" onClick={todoListHandler}>
         Add
       </button>
       <ul>
-        {todoState.todoList.map(todo => (
+        {state.todoList.map(todo => (
           <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
